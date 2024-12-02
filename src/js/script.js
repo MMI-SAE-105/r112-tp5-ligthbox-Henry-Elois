7290c50d0ff9a203bce18225607436958a6ec11b
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2024-12-02 15:11:13 +0100

    lightbox js

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 // *** LIGHTBOX ***
-Const = document.createElement('.lightbox');
+const LightBoxImg = document.createElement("#Lightbox img").querySelectorAll("[data-full-img]").forEach ((imgThumb)=>{
+    imgThumb.addEventListener("click", (evt)=>{
+        LightBoxImg.src = imgThumb.dataset.fullImg;
+    });
+}); 
+
 lightBox.showModal (); 
 lightBox.close ();
 
